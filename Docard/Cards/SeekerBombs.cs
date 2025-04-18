9b66ca164efb20a4f46307f9b3cf28ee54aca956
Doboom <bobgibson987@gmail.com>
2025-04-17 21:23:35 -0400

    Initial commit
    Initial commit
    
    Icon+Manifest added
    
    gitignore fix

---
+++ Docard/Cards/SeekerBombs.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Photon.Pun.Simple;
+using UnboundLib;
+using UnboundLib.Cards;
+using UnityEngine;
+using static CardInfoStat;
+using static UnityEngine.Random;
+
+
+namespace Docard.Cards
+{
+    class SeekerBombs : CustomCard
+    {
+        public override void SetupCard(CardInfo cardInfo, Gun gun, ApplyCardStats cardStats, CharacterStatModifiers statModifiers, Block block)
+        {
+            //Edits values on card itself, which are then applied to the player in `ApplyCardStats`
+            var homing = (GameObject)Resources.Load("0 cards/Homing");
+            var homingObj = homing.GetComponent<Gun>().objectsToSpawn[0];
+
+            gun.objectsToSpawn = new[]
+            {
+                homingObj
+            };
+        }
+        public override void OnAddCard(Player player, Gun gun, GunAmmo gunAmmo, CharacterData data, HealthHandler health, Gravity gravity, Block block, CharacterStatModifiers characterStats)
+        {
+            //Edits values on player when card is selected
+            gun.gravity = 0f;
+            gun.projectileSpeed *= .1f;
+            gun.damage *= 3f;
+            gun.attackSpeed *= 1.25f;
+            gun.reloadTime *= 1.5f;
+            
+        }
+        public override void OnRemoveCard(Player player, Gun gun, GunAmmo gunAmmo, CharacterData data, HealthHandler health, Gravity gravity, Block block, CharacterStatModifiers characterStats)
+        {
+            //Run when the card is removed from the player
+        }
+
+
+        protected override string GetTitle()
+        {
+            return "Seeker Bombs";
+        }
+        protected override string GetDescription()
+        {
+            return "Your bullets are now slow floating bombs that seek players";
+        }
+        protected override GameObject GetCardArt()
+        {
+            return null;
+        }
+        protected override CardInfo.Rarity GetRarity()
+        {
+            return CardInfo.Rarity.Rare;
+        }
+        protected override CardInfoStat[] GetStats()
+        {
+            return new CardInfoStat[]
+            {
+                new CardInfoStat()
+                 {
+                    positive = true,
+                    stat = "Bullet Gravity",
+                    amount = "No",
+                    simepleAmount = CardInfoStat.SimpleAmount.aLotLower
+                },
+                new CardInfoStat()
+                {
+                    positive = false,
+                    stat = "Bullet Speed",
+                    amount = "-90%",
+                    simepleAmount = CardInfoStat.SimpleAmount.aLotLower
+                },
+                new CardInfoStat()
+                {
+                    positive = true,
+                    stat = "Damage",
+                    amount = "+200%",
+                    simepleAmount = CardInfoStat.SimpleAmount.aLotOf
+                },
+                new CardInfoStat()
+                {
+                    positive = false,
+                    stat = "Attack Speed",
+                    amount = "-25%",
+                    simepleAmount = CardInfoStat.SimpleAmount.slightlyLower
+                },
+                new CardInfoStat()
+                {
+                    positive = false,
+                    stat = "Reload Speed",
+                    amount = "+0.5s",
+                    simepleAmount = CardInfoStat.SimpleAmount.Some
+                }
+            };
+        }
+        protected override CardThemeColor.CardThemeColorType GetTheme()
+        {
+            return CardThemeColor.CardThemeColorType.TechWhite;
+        }
+        public override string GetModName()
+        {
+            return Docard.ModInitials;
+        }
+    }
+}
